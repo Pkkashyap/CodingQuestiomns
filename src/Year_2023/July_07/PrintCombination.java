4b740ec5d9f61827c046d83958a26c3f20291f89
Pankaj <pkkashyap616@gmail.com>
2023-07-09 21:42:16 +0530

    check

---
+++ src/Year_2023/July_07/PrintCombination.java
@@ -0,0 +1,21 @@
+package Year_2023.July_07;
+
+public class PrintCombination {
+    public static void print(String s){
+        int n = s.length();
+        int power = 1<<n;
+        for(int i=0;i<power;i++){
+            for(int j=0;j<n;j++){
+                if((i&(1<<j))!=0){
+                    System.out.print(s.charAt(j));
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+
+    public static void main(String args[]){
+        print("av");
+    }
+}
