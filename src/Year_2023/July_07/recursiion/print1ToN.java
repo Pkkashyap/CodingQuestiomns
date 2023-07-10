e3014e7581455c8aa275dacd61274693b4216625
Pankaj <pkkashyap616@gmail.com>
2023-07-10 14:53:25 +0530

    Second day code for simple recursion

---
+++ src/Year_2023/July_07/recursiion/print1ToN.java
@@ -0,0 +1,25 @@
+package Year_2023.July_07.recursiion;
+
+public class print1ToN {
+    public static void print(int n,int k){
+        if(n==0){
+            return;
+        }
+        System.out.print(k+" ");
+
+        print(n-1,k+1);
+    }
+    public static void printReverse(int n){
+        if(n==0){
+            return;
+        }
+        System.out.print(n+" ");
+
+        printReverse(n-1);
+    }
+
+    public static void main(String[] args) {
+        print(5,1);
+        printReverse(5);
+    }
+}
